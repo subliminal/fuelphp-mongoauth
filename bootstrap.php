7fcab655c2ac59f8da2f242e6d69ab0bb2e2375f
Justin Hall <justin.hall@fouro.me>
2012-03-17 20:08:10 -0700

    updating license on files

--- bootstrap.php
+++ bootstrap.php
@@ -1,4 +1,24 @@
 <?php
+/**
+ * MongoAuth is a port (with modifications) of the SimpleAuth library included with FuelPHP
+ *
+ * @package    MongoAuth
+ * @version    1.0
+ * @author     Justin Hall
+ * @license    MIT License
+ * @copyright  2010 - 2011 Justin Hall
+ */
+
+/**
+ * Fuel is a fast, lightweight, community driven PHP5 framework.
+ *
+ * @package    Fuel
+ * @version    1.0
+ * @author     Fuel Development Team
+ * @license    MIT License
+ * @copyright  2010 - 2011 Fuel Development Team
+ * @link       http://fuelphp.com
+ */
 
 Autoloader::add_classes(array(
         'Auth\\Auth_Acl_MongoAcl'  => __DIR__.'/classes/auth/acl/mongoacl.php',
